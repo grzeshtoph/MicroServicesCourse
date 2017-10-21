5c86c751496b632401e084ef07b7a19657da2bdd
greg.smorag <greg.smorag@gmail.com>
2017-10-21 21:19:56 +0100

    lab-3 first commit

---
+++ lab-3/lab-3-server/src/main/java/com/course/labs/lab3server/Lab3ServerApplication.java
@@ -0,0 +1,13 @@
+package com.course.labs.lab3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ServerApplication.class, args);
+	}
+}
