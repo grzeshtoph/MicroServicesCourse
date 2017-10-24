4629a64684d055950ae69ca044950c169a3474a5
greg.smorag <greg.smorag@gmail.com>
2017-10-24 21:28:30 +0100

    lab-5 load balanced sentence service with 2 different instances of noun service

--- lab-5/lab-5-sentence/src/main/java/com/course/labs/sentence/Lab5SentenceApplication.java
+++ lab-5/lab-5-sentence/src/main/java/com/course/labs/sentence/Lab5SentenceApplication.java
@@ -3,6 +3,9 @@ package com.course.labs.sentence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,11 @@ public class Lab5SentenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab5SentenceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- lab-5/lab-5-sentence/src/main/java/com/course/labs/sentence/SentenceController.java
+++ lab-5/lab-5-sentence/src/main/java/com/course/labs/sentence/SentenceController.java
@@ -1,19 +1,14 @@
 package com.course.labs.sentence;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
-
 @RestController
 public class SentenceController {
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private RestTemplate restTemplate;
 
     @GetMapping("/sentence")
     public String getSentence() {
@@ -26,16 +21,7 @@ public class SentenceController {
     }
 
     private String getWord(String serviceName) {
-        List<ServiceInstance> list = discoveryClient.getInstances(serviceName);
-
-        if (list != null && list.size() > 0) {
-            URI uri = list.get(0).getUri();
-            if (uri != null) {
-                return new RestTemplate().getForObject(uri, String.class);
-            }
-        }
-
-        return "";
+        return restTemplate.getForObject("http://" + serviceName, String.class);
 
     }
 }
