950239b5732ac42687dc97c5ce5f8b6ed536c932
greg.smorag <greg.smorag@gmail.com>
2017-10-28 14:06:21 +0100

    lab-8 @ConfigProperties commented out

--- lab-8/client/src/main/java/demo/LuckyWordController.java
+++ lab-8/client/src/main/java/demo/LuckyWordController.java
@@ -1,40 +1,34 @@
 package demo;
 
-import lombok.Getter;
-import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@ConfigurationProperties("word-config")
-public class LuckyWordController {
-	@Getter
-	@Setter
-	String luckyWord;
+import javax.annotation.PostConstruct;
 
-	@Getter
-	@Setter
-	String preamble;
+@RestController
+//@ConfigurationProperties("wordConfig")
+@RefreshScope
+@Scope("prototype")
+public class LuckyWordController {
+	@Value("${wordConfig.luckyWord}")
+	private String luckyWord;
+
+	@Value("${wordConfig.preamble}")
+	private String preamble;
+
+	private String fullSentence;
+
+	@PostConstruct
+	public void init() {
+		fullSentence = preamble + ": " + luckyWord;
+	}
 	
 	@GetMapping("/lucky-word")
 	public String showLuckyWord() {
-		return preamble + ": " + luckyWord;
-	}
-
-	public String getLuckyWord() {
-		return luckyWord;
-	}
-
-	public void setLuckyWord(String luckyWord) {
-		this.luckyWord = luckyWord;
-	}
-
-	public String getPreamble() {
-		return preamble;
-	}
-
-	public void setPreamble(String preamble) {
-		this.preamble = preamble;
+		return fullSentence;
 	}
 }
